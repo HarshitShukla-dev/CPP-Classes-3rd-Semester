a8ee93ca8e6f51976cb087dc376ac0eb7fb01657
hershit-shukla <hershitshukla35@gmail.com>
2023-08-02 15:08:27 +0000

    Aug 2, 2023, 8:38 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
     double myDoubleNum = 5.12345678987;
     float myFloatNum = 5.12345678987;
-    cout << setprecision(9) << fixed;
+    cout << setprecision(5);
     cout << "double: " << myDoubleNum << "\n";
     cout << "float: " << myFloatNum << "\n";
     return 0;
