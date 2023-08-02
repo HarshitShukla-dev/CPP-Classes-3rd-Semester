d757a8aac9498742662cfff7103214cd791f1904
hershit-shukla <hershitshukla35@gmail.com>
2023-08-02 14:56:33 +0000

    Aug 2, 2023, 8:26 PM

--- 02-08-2023.cpp
+++ 02-08-2023.cpp
@@ -1,1 +1,15 @@
 // Different data types
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    float b = 5.5;
+    char c = 'A';
+    bool d = true;
+    cout << "The value of a is " << a << endl;
+    cout << "The value of b is " << b << endl;
+    cout << "The value of c is " << c << endl;
+    cout << "The value of d is " << d << endl;
+    return 0;
+}
