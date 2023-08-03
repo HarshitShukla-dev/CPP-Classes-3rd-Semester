63abb511f4b4a35075ab413ab42162afbd09f586
hershit-shukla <hershitshukla35@gmail.com>
2023-08-03 14:17:03 +0000

    Aug 3, 2023, 7:47 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int a = 10;
-    cout << "a++ = " << a++ << endl;
-    cout << "Now a = " << a << endl;
-    cout << "++a = " << ++a << endl;
-    cout << "Now a = " << a << endl;
-    cout << "a-- = " << a-- << endl;
-    cout << "Now a = " << a << endl;
-    cout << "--a = " << --a << endl;
-    cout << "Now a = " << a << endl;
+int main()
+{
+    int a = 6, b = 4;
+    // Equal to
+    cout << "a == b = " << (a == b) << endl;
+    // Less than
+    cout << "a < b = " << (a < b) << endl;
+    // Greater than
+    cout << "a > b = " << (a > b) << endl;
+    // Less than or equal to
+    cout << "a <= b = " << (a <= b) << endl;
+    // Greater than or equal to
+    cout << "a >= b = " << (a >= b) << endl;
+    // Not equal to
+    cout << "a != b = " << (a != b) << endl;
     return 0;
 }
