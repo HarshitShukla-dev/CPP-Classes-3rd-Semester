de9df3557f8c05fb5ae2f4c8db2e0646f63924bd
hershit-shukla <hershitshukla35@gmail.com>
2023-08-03 13:53:07 +0000

    Aug 3, 2023, 7:23 PM

---
+++ 03-08-2023 Operators.cpp
@@ -0,0 +1,19 @@
+// Operators
+
+// 1. Arithmetic Operators
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 8, b = 3;
+    // Addition
+    cout << "a + b = " << a + b << endl;
+    // Subtraction
+    cout << "a - b = " << a - b << endl;
+    // Multiplication
+    cout << "a * b = " << a * b << endl;
+    // Division
+    cout << "a / b = " << a / b << endl;
+    // Modulus
+    cout << "a % b = " << a % b << endl;
+}
