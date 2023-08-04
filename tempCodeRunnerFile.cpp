31868449a04cf59bf9d17091a8cdffb559458804
hershit-shukla <hershitshukla35@gmail.com>
2023-08-04 15:15:19 +0000

    Aug 4, 2023, 8:45 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main()
 {
-    for (int i = 1; i <= 100; i++) //
+    int n;
+    cin >> n;
+    for (int i = 1; i <= 10; ++i)
     {
-        cout << "I got Placed in Google" << endl;
+        cout << n << " * " << i << " = " << n * i << endl;
     }
     return 0;
 }
