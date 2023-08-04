f9e3890b8b917faa23616ff1284779faef5d1fa6
hershit-shukla <hershitshukla35@gmail.com>
2023-08-04 13:46:16 +0000

    Aug 4, 2023, 7:16 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,11 +1,9 @@
-
 #include <iostream>
 using namespace std;
 int main()
 {
-    int a = 3, b = 4, result;
-    // Ternary operator
-    result = (a < b) ? a : b; // If a is greater than b, then c = a else c = b
-    cout << "result = " << result << endl;
+    int a, b, c;
+   //cin >> a >> b >> c;
+    cout << a << " " << b << " " << c;
     return 0;
 }
