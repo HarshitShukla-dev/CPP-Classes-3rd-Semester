5e438b7e2ea8e5cce62ba34216b1e518f16e916b
hershit-shukla <hershitshukla35@gmail.com>
2023-08-07 14:13:21 +0000

    Aug 7, 2023, 7:43 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main()
 {
-    int number, temp, reverse = 0;
+    int number, temp, newNumber = 0;
     cout << "Enter an integer: ";
     cin >> number;
     while (number != 0)
     {
         temp = number % 10;
-        reverse = reverse * 10 + temp;
+        newNumber = newNumber * 100 + temp * temp;
         number /= 10;
     }
-    cout << reverse << endl;
+    cout << newNumber << endl;
     return 0;
 }
