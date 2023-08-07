e1eda3995cd8316c11fa0633e33a394e58617e44
hershit-shukla <hershitshukla35@gmail.com>
2023-08-07 15:17:04 +0000

    Aug 7, 2023, 8:47 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -7,9 +7,6 @@ int main() {
     cin >> rows;
 
     for (int i = 1; i <= rows; ++i) {
-        for (int j = 1; j <= rows - i; ++j) {
-            cout << " ";
-        }
         for (int j = 1; j <= 2 * i - 1; ++j) {
             cout << (j % 2);
         }
