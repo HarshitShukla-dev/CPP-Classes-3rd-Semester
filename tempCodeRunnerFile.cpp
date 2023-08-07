6bee15f0619b59f6c80c2033d5a422d47743c12b
hershit-shukla <hershitshukla35@gmail.com>
2023-08-07 15:16:43 +0000

    Aug 7, 2023, 8:46 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,17 +1,21 @@
-//print opposite triangle using numbers
 #include <iostream>
 using namespace std;
-int main()
-{
-    int n;
-    cin >> n;
-    for (int i = n; i >= 1; i--) 
-    {
-        for (int j = 1; j <= i; j++) 
-        {
-            cout << i << " ";
+
+int main() {
+    int rows;
+
+    cin >> rows;
+
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= rows - i; ++j) {
+            cout << " ";
         }
-        cout << endl;
+        for (int j = 1; j <= 2 * i - 1; ++j) {
+            cout << (j % 2);
+        }
+
+        cout << std::endl;
     }
+
     return 0;
 }
