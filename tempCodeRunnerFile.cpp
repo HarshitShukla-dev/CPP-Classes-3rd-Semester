e272f53deedc6b9e7a78ae9dda8abdcb14da9b61
hershit-shukla <hershitshukla35@gmail.com>
2023-08-07 14:18:16 +0000

    Aug 7, 2023, 7:48 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,13 +3,12 @@ using namespace std;
 int main()
 {
     int n;
-    cout << "Enter the number of rows: ";
     cin >> n;
     for (int i = 1; i <= n; i++) //outer loop
     {
         for (int j = 1; j <= n; j++) //inner loop
         {
-            cout << "*";
+            cout << "* ";
         }
         cout << endl;
     }
