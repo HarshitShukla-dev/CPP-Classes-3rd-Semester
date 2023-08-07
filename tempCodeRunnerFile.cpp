6995ca5e1dee362d222e6fc91396f87561c060ea
hershit-shukla <hershitshukla35@gmail.com>
2023-08-07 13:46:05 +0000

    Aug 7, 2023, 7:16 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
-    for (int i = 1; i <= 10; ++i)
+    int number, sumEven = 0, sumOdd = 0;
+    cout << "Enter an integer: ";
+    cin >> number;
+    while (number != 0)
     {
-        cout << n << " * " << i << " = " << n * i << endl;
+        if ((number % 10) % 2 == 0)
+        {
+            sumEven += number % 10;
+        }
+        else
+        {
+            sumOdd += number % 10;
+        }
+        number /= 10;
     }
+    cout << "Sum of even digits: " << sumEven << endl;
+    cout << "Sum of odd digits: " << sumOdd << endl;
     return 0;
 }
