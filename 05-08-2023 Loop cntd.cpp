82d2dc5bc91afa9c662ea877c935c1a1c6624f7b
hershit-shukla <hershitshukla35@gmail.com>
2023-08-07 14:52:18 +0000

    Aug 7, 2023, 8:22 PM

--- 05-08-2023 Loop cntd.cpp
+++ 05-08-2023 Loop cntd.cpp
@@ -125,3 +125,39 @@ int main()
     return 0;
 }
 
+// Print triangle using stars
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = 1; j <= i; j++) 
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// Print triangle using numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) 
+    {
+        for (int j = 1; j <= i; j++) 
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
