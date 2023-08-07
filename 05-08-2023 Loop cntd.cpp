51b616b3be7c391109b812d3c18d1f421e2f96ef
hershit-shukla <hershitshukla35@gmail.com>
2023-08-07 14:00:11 +0000

    Aug 7, 2023, 7:30 PM

--- 05-08-2023 Loop cntd.cpp
+++ 05-08-2023 Loop cntd.cpp
@@ -47,3 +47,21 @@ int main()
     cout << "Sum of odd digits: " << sumOdd << endl;
     return 0;
 }
+
+// Take a number input and reverse it
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number, temp, reverse = 0;
+    cout << "Enter an integer: ";
+    cin >> number;
+    while (number != 0)
+    {
+        temp = number % 10;
+        reverse = reverse * 10 + temp;
+        number /= 10;
+    }
+    cout << reverse << endl;
+    return 0;
+}
