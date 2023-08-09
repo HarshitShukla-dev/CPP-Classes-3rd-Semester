a4b1e8035ec24c7321d3de86d031d80eaa76e09b
hershit-shukla <hershitshukla35@gmail.com>
2023-08-09 14:06:19 +0000

    Aug 9, 2023, 7:36 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,22 +1,9 @@
-#include <iostream>
-using namespace std;
-
-int Factorial(int num)
-{
-    int fact = 1;
-    for (int i = 1; i <= num; i++)
-    {
-        fact *= num;
-    }
-    return fact;
-}
-
-int main()
-{
-    int n, r, ncr;
-    cout << "Enter n and r: ";
-    cin >> n >> r;
-    ncr = Factorial(n) / (Factorial(r) * Factorial(n - r));
-    cout << "nCr = " << ncr << endl;
-    return 0;
-}
+void B(){
+cout<<3; }
+void A(){  
+B();
+cout<<2;  }
+int main(){
+A();
+cout<<1;  
+return 0;}
