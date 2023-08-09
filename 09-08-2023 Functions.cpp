926ade7326528159ab48ceb0c47da336b2bbf05d
hershit-shukla <hershitshukla35@gmail.com>
2023-08-09 14:08:47 +0000

    Aug 9, 2023, 7:38 PM

--- 09-08-2023 Functions.cpp
+++ 09-08-2023 Functions.cpp
@@ -37,3 +37,21 @@ int main()
 }
 
 //______________
+
+#include <iostream>
+using namespace std;
+void B()
+{
+    cout << 3;
+}
+void A()
+{
+    B();
+    cout << 2;
+}
+int main()
+{
+    A();
+    cout << 1;
+    return 0;
+}
