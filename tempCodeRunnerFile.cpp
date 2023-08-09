1009573c2404baddd3dd569ad3ea78e326ac028d
hershit-shukla <hershitshukla35@gmail.com>
2023-08-09 14:07:11 +0000

    Aug 9, 2023, 7:37 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,15 @@
-void B(){
-cout<<3; }
-void A(){  
-B();
-cout<<2;  }
-int main(){
-A();
-cout<<1;  
-return 0;}
+void B()
+{
+    cout << 3;
+}
+void A()
+{
+    B();
+    cout << 2;
+}
+int main()
+{
+    A();
+    cout << 1;
+    return 0;
+}
