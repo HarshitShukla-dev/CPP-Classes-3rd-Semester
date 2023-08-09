442882fd272cb66430bd6b8dc79fa91bdc98741a
hershit-shukla <hershitshukla35@gmail.com>
2023-08-09 13:47:17 +0000

    Aug 9, 2023, 7:17 PM

---
+++ 09-08-2023 Functions.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+using namespace std;
+
+int Factorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= num;
+    }
+    return fact;
+}
+
+int main()
+{
+    int n, r, ncr;
+    cout << "Enter n and r: ";
+    cin >> n >> r;
+    ncr = Factorial(n) / (Factorial(r) * Factorial(n - r));
+    cout << "nCr = " << ncr << endl;
+    return 0;
+}
