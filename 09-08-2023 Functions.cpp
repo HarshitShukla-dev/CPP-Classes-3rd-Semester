565426e64fb30e0ee2a7f5b6e7359b05e07e1e76
hershit-shukla <hershitshukla35@gmail.com>
2023-08-09 13:51:40 +0000

    Aug 9, 2023, 7:21 PM

--- 09-08-2023 Functions.cpp
+++ 09-08-2023 Functions.cpp
@@ -1,4 +1,13 @@
+// Functions
+/*
+Functions in C++ are used to divide a big program into small parts. It makes the program more manageable and organized. It also avoids repetition and makes the code reusable. Functions in C++ are also known as methods or procedures.
 
+There are two types of functions in C++: library functions and user-defined functions.
+
+Library functions are the functions which are already defined in C++ libraries like iostream, math, string, etc. We just need to include the header file of the library in which the function is defined and call the function to use it.
+
+User-defined functions are the functions which are created by the user at the time of writing the program. These functions are created to perform some specific tasks. We can create user-defined functions according to our requirements.
+*/
 
 #include <iostream>
 using namespace std;
