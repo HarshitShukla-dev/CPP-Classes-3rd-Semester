185051e2c83bf9a2e5f054cbdc668883481301da
hershit-shukla <hershitshukla35@gmail.com>
2023-08-16 14:46:03 +0000

    Aug 16, 2023, 8:16 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 using namespace std;
-void B()
-{
-    cout << 3;
-}
-void A()
-{
-    B();
-    cout << 2;
-}
-int main()
-{
-    A();
-    cout << 1;
+int main(){
+    int n, countZero = 0, countOne = 0;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        int temp;
+        cin >> temp;
+        arr[i] = temp;
+        if (temp == 0) countZero += 1;
+        else if (temp == 1) countOne += 1;
+        else continue;
+    }
+
+    int reArr[n];
+
+    for(int i=0;i<countZero;i++){
+        reArr[i] = 0;
+    }
+    for(int i=countZero;i<countZero+countOne;i++){
+        reArr[i] = 1;
+    }
+
     return 0;
+
+
 }
