04692acabc416e7b2e6ed683a5fc74e9a50502e5
hershit-shukla <hershitshukla35@gmail.com>
2023-08-16 15:00:24 +0000

    Aug 16, 2023, 8:30 PM

--- 16-08-2023 Pointers cntd.cpp
+++ 16-08-2023 Pointers cntd.cpp
@@ -1,4 +1,4 @@
-//Double Pointer
+// Double Pointer
 
 /*
 Double pointer refers to a pointer pointing to another pointer.
@@ -8,7 +8,8 @@ It is used to store the address of a pointer as a value.
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter the number of digits: ";
     cin >> n;
@@ -17,30 +18,34 @@ int main() {
     int countZeros = 0;
 
     cout << "Enter " << n << " binary digits (0 or 1) separated by spaces:" << endl;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         int digit;
         cin >> digit;
         inputArray[i] = digit;
-        
-        if (digit == 0) 
+
+        if (digit == 0)
             countZeros++;
     }
 
     int rearrangedArray[n];
 
     // Place zeros at the beginning
-    for (int i = 0; i < countZeros; i++) {
+    for (int i = 0; i < countZeros; i++)
+    {
         rearrangedArray[i] = 0;
     }
 
     // Place ones after zeros
-    for (int i = countZeros; i < n; i++) {
+    for (int i = countZeros; i < n; i++)
+    {
         rearrangedArray[i] = 1;
     }
 
     // Output the rearranged array
     cout << "Rearranged array: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << rearrangedArray[i] << " ";
     }
     cout << endl;
@@ -50,3 +55,23 @@ int main() {
 
 //___________________
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of digits: ";
+    cin >> n;
+
+    int inputArray[n];
+    int countZeros = 0;
+
+    cout << "Enter " << n << " binary digits (0 or 1) separated by spaces:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int digit;
+        cin >> digit;
+        inputArray[i] = digit;
+    }
+}
