5b7d9b9f797202474f6326f776831f52035c4a3d
hershit-shukla <hershitshukla35@gmail.com>
2023-08-16 15:13:48 +0000

    Aug 16, 2023, 8:43 PM

--- 16-08-2023 Pointers cntd.cpp
+++ 16-08-2023 Pointers cntd.cpp
@@ -99,3 +99,34 @@ int main()
 
 //___________________
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        int temp;
+        cin >> temp;
+        arr[i] = temp;
+    }
+    int dblINT = 0, num = 0;
+    for (int i = 0; i  <n; i++){
+        for (int j=0; j<n;j++){
+            if (arr[i] == arr[j] && i != j){
+                dblINT += 1;
+                num = arr[i];
+                break;
+            }
+        }
+    }
+
+    if (dblINT == 0){
+        cout << "No duplicate elements found" << endl;
+    }
+    else{
+        cout << "Duplicate element found: " << num << endl;
+    }
+    return 0;
+}
