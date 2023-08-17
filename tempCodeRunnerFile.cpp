4447a860d48532cca7f905debe5b38d3c52b49f0
hershit-shukla <hershitshukla35@gmail.com>
2023-08-17 14:03:51 +0000

    Aug 17, 2023, 7:33 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,28 @@
-#include<iostream>
+// Ascii value of null charecter '\0' is 0
+
+#include <iostream>
 using namespace std;
-int main(){
-    char a[100];
+
+// Function to calculate the length of a C-style string
+int calculateLength(const char *str) {
     int count = 0;
-    cin>>a;
-    cout<<a<<endl;
-
-    for(int i = 0; i <100; i++){
-        if (a[i] == '\0'){
-            
-            break;
+    for (int i = 0; str[i] != '\0'; i++) {
+        count++;
     }
-    count += 1;
-    }
+    return count;
+}
 
-    cout<<"length : "<<count<<endl;
+int main() {
+    char a[100];
+
+    cout << "Enter a string: ";
+    cin >> a; // Reading input as a C-style string
+
+    cout << "Entered string: " << a << endl;
+
+    int length = calculateLength(a); // Call the length calculation function
+
+    cout << "Length: " << length << endl;
+
     return 0;
 }
