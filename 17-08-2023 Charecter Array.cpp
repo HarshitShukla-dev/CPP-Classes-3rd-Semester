558ff44e46382892930b4f2c5e1d0fdfe3e7fe33
hershit-shukla <hershitshukla35@gmail.com>
2023-08-17 14:10:48 +0000

    Aug 17, 2023, 7:40 PM

--- 17-08-2023 Charecter Array.cpp
+++ 17-08-2023 Charecter Array.cpp
@@ -30,3 +30,7 @@ int main() {
 
 // cin reads a string until it encounters a whitespace character (space, newline, tab, etc.).
 // cin.getline reads a string of characters until it encounters a newline character.
+// cin.get reads a string of characters until it encounters a newline character, but extracts the newline character from the input sequence and stores it in c.
+// cin.ignore ignores the newline character extracted by cin.get and clears the input buffer.
+// cin.getline and cin.get are used to read strings with blank spaces.
+// syntax: cin.getline(char_array, size_of_array, delimiter) where delimiter is optional and by default is '\n'. By deilimiter we mean the character after which cin.getline will stop reading the input.
