b5962d0168cbc8de8346c9f0ab93a6fb57f5eaf9
hershit-shukla <hershitshukla35@gmail.com>
2023-08-17 14:12:10 +0000

    Aug 17, 2023, 7:42 PM

--- 17-08-2023 Charecter Array.cpp
+++ 17-08-2023 Charecter Array.cpp
@@ -34,3 +34,30 @@ int main() {
 // cin.ignore ignores the newline character extracted by cin.get and clears the input buffer.
 // cin.getline and cin.get are used to read strings with blank spaces.
 // syntax: cin.getline(char_array, size_of_array, delimiter) where delimiter is optional and by default is '\n'. By deilimiter we mean the character after which cin.getline will stop reading the input.
+
+#include <iostream>
+using namespace std;
+
+// Function to calculate the length of a C-style string
+int calculateLength(const char *str) {
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    char a[100];
+
+    cout << "Enter a string: ";
+    cin.getline(a, 100); // Reading input as a C-style string
+
+    cout << "Entered string: " << a << endl;
+
+    int length = calculateLength(a); // Call the length calculation function
+
+    cout << "Length: " << length << endl;
+
+    return 0;
+}
