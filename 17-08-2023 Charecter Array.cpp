7dd27eb5b50215f43cf8e11ef9709439e8a8a1b7
hershit-shukla <hershitshukla35@gmail.com>
2023-08-17 14:01:19 +0000

    Aug 17, 2023, 7:31 PM

--- 17-08-2023 Charecter Array.cpp
+++ 17-08-2023 Charecter Array.cpp
@@ -1,17 +1,28 @@
 // Ascii value of null charecter is 0
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    char a[100];
+
+// Function to calculate the length of a C-style string
+int calculateLength(const char *str) {
     int count = 0;
-    cin>>a;
-    cout<<a<<endl;
-
-    for(int i = 0; a[i] != '\0'; i++){
-        count += 1;
+    for (int i = 0; str[i] != '\0'; i++) {
+        count++;
     }
+    return count;
+}
 
-    cout<<"length : "<<count<<endl;
+int main() {
+    char a[100];
+
+    cout << "Enter a string: ";
+    cin >> a; // Reading input as a C-style string
+
+    cout << "Entered string: " << a << endl;
+
+    int length = calculateLength(a); // Call the length calculation function
+
+    cout << "Length: " << length << endl;
+
     return 0;
 }
