b31ec05179d2f6af7f3df81a19ec4cca4453c9c9
hershit-shukla <hershitshukla35@gmail.com>
2023-08-17 14:16:05 +0000

    Aug 17, 2023, 7:46 PM

--- 17-08-2023 Charecter Array.cpp
+++ 17-08-2023 Charecter Array.cpp
@@ -64,3 +64,19 @@ int main() {
 
 //______________
 
+#include <iostream>
+using namespace std;
+int main(){
+    char a[100];
+    int count = 0;
+    cin.getline(a, 100);
+    cout<<a<<endl;
+
+    for(int i = 0; i<100; i++){
+        if (a[i] == ' ')
+        count += 1;
+    }
+
+    cout<<"spaces : "<<count<<endl;
+    return 0;
+}
