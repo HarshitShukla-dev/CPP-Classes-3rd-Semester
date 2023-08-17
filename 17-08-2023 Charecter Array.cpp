427978de3fdaa129d5147b3a770f861ed2f8c9df
hershit-shukla <hershitshukla35@gmail.com>
2023-08-17 13:56:22 +0000

    Aug 17, 2023, 7:26 PM

--- 17-08-2023 Charecter Array.cpp
+++ 17-08-2023 Charecter Array.cpp
@@ -1,1 +1,17 @@
 // Ascii value of null charecter is 0
+
+#include<iostream>
+using namespace std;
+int main(){
+    char a[100];
+    int count = 0;
+    cin>>a;
+    cout<<a<<endl;
+
+    for(int i = 0; a[i] != '\0'; i++){
+        count += 1;
+    }
+
+    cout<<"length : "<<count<<endl;
+    return 0;
+}
