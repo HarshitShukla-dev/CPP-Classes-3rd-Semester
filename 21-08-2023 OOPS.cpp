fb6eec9f21d5e5fce9249ba30e4ebaadb0ac7ba5
hershit-shukla <hershitshukla35@gmail.com>
2023-08-21 13:46:22 +0000

    Aug 21, 2023, 7:16 PM

--- 21-08-2023 OOPS.cpp
+++ 21-08-2023 OOPS.cpp
@@ -1,1 +1,18 @@
 // Object Oriented Programming
+/*
+object is a real world entity which has state and behavior.
+class is a blueprint of an object.
+class is a user defined data type.
+class is a collection of objects.
+class is a logical entity.
+class is a template.
+class is a group of objects with common properties.
+class is a group of objects with common behavior.
+class is a group of objects with common state.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+}
