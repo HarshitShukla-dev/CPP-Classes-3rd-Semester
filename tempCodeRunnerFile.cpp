8c98a252b4b142108f78881f96de888a48a1a129
hershit-shukla <hershitshukla35@gmail.com>
2023-08-21 13:56:21 +0000

    Aug 21, 2023, 7:26 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,16 +1,34 @@
+// Object Oriented Programming
+/*
+object is a real world entity which has state and behavior.
+class is a blueprint of an object.
+class is a user defined data type.
+class is a collection of objects.
+class is a logical entity.
+class is a template.
+class is a group of objects with common properties.
+class is a group of objects with common behavior.
+class is a group of objects with common state.
+*/
+
 #include <iostream>
 using namespace std;
-int main(){
-    char a[100];
-    int count = 0;
-    cin.getline(a, 100);
-    cout<<a<<endl;
 
-    for(int i = 0; i<100; i++){
-        if (a[i] == ' ')
-        count += 1;
-    }
+class Student
+{
+public:
+    int rollNumber;
+    int age;
+};
 
-    cout<<"spaces : "<<count<<endl;
+int main()
+{
+    Student s1, s2;
+    s1.rollNumber = 20;
+    s1.age = 24;
+
+    cout << s1.rollNumber << endl;
+    cout << s1.age << endl;
+
     return 0;
 }
