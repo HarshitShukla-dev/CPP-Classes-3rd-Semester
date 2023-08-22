60ce658e3424a7bc0308eb6d75ddeb474f484cc1
hershit-shukla <hershitshukla35@gmail.com>
2023-08-22 15:11:38 +0000

    Aug 22, 2023, 8:41 PM

--- script.py
+++ script.py
@@ -2,14 +2,22 @@ import pyautogui
 import time
 
 def main():
-    print("Enter the text you want to type:")
-    text = input()
+    print("Enter the text you want to type (use Ctrl+D or Ctrl+Z to finish input):")
+    lines = []
+    try:
+        while True:
+            line = input()
+            lines.append(line)
+    except EOFError:
+        pass
 
     print("Switch to the application/window where you want to type.")
     print("You have 5 seconds to focus on the application...")
     time.sleep(5)
 
-    pyautogui.typewrite(text)
+    for line in lines:
+        pyautogui.typewrite(line)
+        pyautogui.press('enter')
 
 if __name__ == "__main__":
     main()
