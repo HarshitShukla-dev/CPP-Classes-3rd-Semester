4336621ea37b7e10e634af863871f7e4766396c5
hershit-shukla <hershitshukla35@gmail.com>
2023-08-22 13:43:55 +0000

    Aug 22, 2023, 7:13 PM

---
+++ 22-08-2023 OOPS cont (destructer).cpp
@@ -0,0 +1,9 @@
+// Destructor : It is a special member function of a class which is executed automatically when an object of the class goes out of scope.
+
+#include <iostream>
+using namespace std;
+
+class Student
+{
+    
+}
