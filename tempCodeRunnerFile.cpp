932508dec90118b24eebd089d7776cbf81a374b1
hershit-shukla <hershitshukla35@gmail.com>
2023-08-29 13:54:35 +0000

    Aug 29, 2023, 7:24 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,37 +1,17 @@
 #include <iostream>
-void mergeArrays(int arr1[], int n, int arr2[], int m, int merged[]) {
-int i = 0, j = 0, k = 0;
-while (i < n && j < m) {
-if (arr1[i] >= arr2[j]) {
-merged[k++] = arr1[i++];
-} else {
-merged[k++] = arr2[j++];
+using namespace std;
+
+int count(int n)
+{
+    if (n == 0)
+        return 0;
+    return 1 + count(n / 10);
 }
-}
-while (j < n) {
-merged[k++] = arr2[j++];
-}
-while (i < m) {
-merged[k++] = arr1[i++];
-}
-}
-int main() {
-int n, m;
-std::cin >> n;
-int arr1[100];
-for (int i = 0; i < n; ++i) {
-std::cin >> arr1[i];
-}
-std::cin >> m;
-int arr2[100];
-for (int i = 0; i < m; ++i) {
-std::cin >> arr2[i];
-}
-int mergedArray[200];
-mergeArrays(arr1, n, arr2, m, mergedArray);
-for (int i = 0; i < n + m; ++i) {
-std::cout << mergedArray[i] << " ";
-}
-std::cout << std::endl;
-return 0;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << count(n);
+    return 0;
 }
