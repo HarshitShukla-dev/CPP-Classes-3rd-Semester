30836dc29065ac32b6e38d9a5f597d8ed87f16a6
hershit-shukla <hershitshukla35@gmail.com>
2023-08-29 13:45:45 +0000

    Aug 29, 2023, 7:15 PM

---
+++ 29-08-2023 Recursion and vector.cpp
@@ -0,0 +1,6 @@
+/* 
+Recusriion is a function calling itself. 
+It is used to solve problems that can be broken down into smaller, repetitive problems.
+For example, factorial of a number can be calculated using recursion as follows:
+n! = n * (n-1)!
+*/
