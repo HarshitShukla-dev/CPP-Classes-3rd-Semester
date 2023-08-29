94041004e2fa9194b536e33f5e2f338ac2b6bf78
hershit-shukla <hershitshukla35@gmail.com>
2023-08-29 13:46:48 +0000

    Aug 29, 2023, 7:16 PM

--- 29-08-2023 Recursion and vector.cpp
+++ 29-08-2023 Recursion and vector.cpp
@@ -4,3 +4,22 @@ It is used to solve problems that can be broken down into smaller, repetitive pr
 For example, factorial of a number can be calculated using recursion as follows:
 n! = n * (n-1)!
 */
+
+#include <iostream>
+using namespace std;
+
+void print(int n)
+{
+    if (n == 0)
+        return;
+    print(n - 1);
+    cout << n << " ";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
