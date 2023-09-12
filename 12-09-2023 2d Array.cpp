f658ac32b0a0c41aab1ea66e11f2b2dc42bbfa1b
hershit-shukla <hershitshukla35@gmail.com>
2023-09-12 14:31:05 +0000

    Sep 12, 2023, 8:01 PM

--- 12-09-2023 2d Array.cpp
+++ 12-09-2023 2d Array.cpp
@@ -163,3 +163,33 @@ int main() {
 
 //_________________________
 
+// Printing a 2d Array in Column Wise
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int arr[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int j = 0; j < m; j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i][j];
+        }
+        cout << "sum of " << j+1 << " column is : " << sum << endl;
+    }
+}
