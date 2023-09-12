e15f2a691f94f3922c8e1c173d7a3c11b7cd6d96
hershit-shukla <hershitshukla35@gmail.com>
2023-09-12 15:02:28 +0000

    Sep 12, 2023, 8:32 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
 
-// program to find maximum sum of sums of all rows and columns of a matrix with that row and column index printed
-
-int main() {
+int main()
+{
     int n, m;
     cin >> n >> m;
 
@@ -11,48 +10,30 @@ int main() {
 
     for (int i = 0; i < n; i++)
     {
-        int sum = 0;
         for (int j = 0; j < m; j++)
         {
             cin >> arr[i][j];
         }
     }
 
-    int maxSum = INT_MIN;
-    int maxRow = 0;
-    int maxCol = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < m; j++)
+        {
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
 
     for (int i = 0; i < n; i++)
     {
-        int sum = 0;
         for (int j = 0; j < m; j++)
         {
-            sum += arr[i][j];
+            cout << arr[i][j] << " ";
         }
-        if (sum > maxSum)
-        {
-            maxSum = sum;
-            maxRow = i;
-        }
+        cout << endl;
     }
-
-    for (int j = 0; j < m; j++)
-    {
-        int sum = 0;
-        for (int i = 0; i < n; i++)
-        {
-            sum += arr[i][j];
-        }
-        if (sum > maxSum)
-        {
-            maxSum = sum;
-            maxCol = j;
-        }
-    }
-
-    cout << "Maximum sum is : " << maxSum << endl;
-    cout << "Row index : " << maxRow << endl;
-    cout << "Column index : " << maxCol << endl;
 
     return 0;
 }
