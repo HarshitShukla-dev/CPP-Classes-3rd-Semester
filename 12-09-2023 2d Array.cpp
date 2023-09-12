a3bcb5424470152cfc2cefcfaa02d357e1aa3534
hershit-shukla <hershitshukla35@gmail.com>
2023-09-12 13:54:35 +0000

    Sep 12, 2023, 7:24 PM

--- 12-09-2023 2d Array.cpp
+++ 12-09-2023 2d Array.cpp
@@ -27,3 +27,29 @@ int main()
 
 //_________________________
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int arr[n][m];
+
+    // Taking input in 2d Array
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    // Printing 2d Array
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
