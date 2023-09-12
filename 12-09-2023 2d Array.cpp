069ae6c9b9c2b698fb59e4f22b34d3f62d2882f6
hershit-shukla <hershitshukla35@gmail.com>
2023-09-12 13:52:09 +0000

    Sep 12, 2023, 7:22 PM

--- 12-09-2023 2d Array.cpp
+++ 12-09-2023 2d Array.cpp
@@ -1,5 +1,29 @@
 /*
-2d Array : 2d array is a collection of 1d array. It is also known as matrix.
+2d Array : 2d array is a collection of 1d array. It is also known as matrix. It is Array inside Array.
 Syntax : data_type array_name[row_size][column_size];
 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3][4] = {{1, 2, 3, 4},
+                     {5, 6, 7, 8},
+                     {9, 0, 1, 2}};
+
+    // Printing 2d Array
+    for (int i = 0; i < 3; i++) // Row
+    {
+        for (int j = 0; j < 4; j++) // Column
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+//_________________________
 
