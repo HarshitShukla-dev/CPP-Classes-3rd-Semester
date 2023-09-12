5c84f24a3e4ebcb50ba84ef8827cc2c135ef7e9c
hershit-shukla <hershitshukla35@gmail.com>
2023-09-12 15:03:36 +0000

    Sep 12, 2023, 8:33 PM

--- 12-09-2023 2d Array.cpp
+++ 12-09-2023 2d Array.cpp
@@ -318,3 +318,47 @@ int main()
 
     return 0;
 }
+
+//_________________________
+
+// program to make transpose of given matrix
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int arr[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < m; j++)
+        {
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
