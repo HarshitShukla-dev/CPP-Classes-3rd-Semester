bcebaa76211c0f5dadd8f96fa4286320bac77df9
hershit-shukla <hershitshukla35@gmail.com>
2023-09-12 14:38:39 +0000

    Sep 12, 2023, 8:08 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+// program to find maximum sum of sums of all rows and columns of a matrix with that row and column index printed
+
 int main() {
     int n, m;
     cin >> n >> m;
@@ -16,6 +18,24 @@ int main() {
         }
     }
 
+    int maxSum = INT_MIN;
+    int maxRow = 0;
+    int maxCol = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < m; j++)
+        {
+            sum += arr[i][j];
+        }
+        if (sum > maxSum)
+        {
+            maxSum = sum;
+            maxRow = i;
+        }
+    }
+
     for (int j = 0; j < m; j++)
     {
         int sum = 0;
@@ -23,6 +43,16 @@ int main() {
         {
             sum += arr[i][j];
         }
-        cout << "sum of " << j+1 << " column is : " << sum << endl;
+        if (sum > maxSum)
+        {
+            maxSum = sum;
+            maxCol = j;
+        }
     }
+
+    cout << "Maximum sum is : " << maxSum << endl;
+    cout << "Row index : " << maxRow << endl;
+    cout << "Column index : " << maxCol << endl;
+
+    return 0;
 }
