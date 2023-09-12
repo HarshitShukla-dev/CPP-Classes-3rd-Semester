1668dfe76b5ac2862912bab1acc2eafa804ce2d1
hershit-shukla <hershitshukla35@gmail.com>
2023-09-12 14:24:25 +0000

    Sep 12, 2023, 7:54 PM

--- 12-09-2023 2d Array.cpp
+++ 12-09-2023 2d Array.cpp
@@ -150,10 +150,10 @@ int main() {
         }
     }
 
-    for (int j = 0; j < m; j++)
+    for (int i = 0; i < n; i++)
     {
         int sum = 0;
-        for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
         {
             sum += arr[i][j];
         }
