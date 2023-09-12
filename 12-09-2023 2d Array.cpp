7ff4b68054e065aac2bfac302c49d9150746a93d
hershit-shukla <hershitshukla35@gmail.com>
2023-09-12 14:49:44 +0000

    Sep 12, 2023, 8:19 PM

--- 12-09-2023 2d Array.cpp
+++ 12-09-2023 2d Array.cpp
@@ -122,7 +122,8 @@ int main()
     {
         for (int j = 0; j < m; j++)
         {
-            cout << "row : " << i << " column : " << " " << arr[i][j] << " ";
+            cout << "row : " << i << " column : "
+                 << " " << arr[i][j] << " ";
         }
         cout << endl;
     }
@@ -135,7 +136,8 @@ int main()
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n, m;
     cin >> n >> m;
 
@@ -157,7 +159,7 @@ int main() {
         {
             sum += arr[i][j];
         }
-        cout << "sum of " << i+1 << " row is : " << sum << endl;
+        cout << "sum of " << i + 1 << " row is : " << sum << endl;
     }
 }
 
@@ -168,7 +170,8 @@ int main() {
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n, m;
     cin >> n >> m;
 
@@ -199,7 +202,8 @@ int main() {
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n, m;
     cin >> n >> m;
 
@@ -221,7 +225,7 @@ int main() {
         {
             sum += arr[i][j];
         }
-        cout << "sum of " << j+1 << " column is : " << sum << endl;
+        cout << "sum of " << j + 1 << " column is : " << sum << endl;
     }
 
     return 0;
@@ -229,3 +233,88 @@ int main() {
 
 //_________________________
 
+// program to find maximum sum of sums of all rows and columns of a matrix with those rows and columns index printed
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cout << "Enter the number of rows and columns: ";
+    cin >> n >> m;
+
+    int matrix[100][100];
+
+    cout << "Enter the elements of the matrix:" << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int maxSum = INT_MIN;
+    int maxRows[100];
+    int maxCols[100];
+    int maxRowCount = 0;
+    int maxColCount = 0;
+
+    // Calculate the sum of each row and find the maximum
+    for (int i = 0; i < n; ++i)
+    {
+        int rowSum = 0;
+        for (int j = 0; j < m; ++j)
+        {
+            rowSum += matrix[i][j];
+        }
+        if (rowSum > maxSum)
+        {
+            maxSum = rowSum;
+            maxRowCount = 1;
+            maxRows[0] = i;
+        }
+        else if (rowSum == maxSum)
+        {
+            maxRows[maxRowCount] = i;
+            maxRowCount++;
+        }
+    }
+
+    // Calculate the sum of each column and find the maximum
+    for (int j = 0; j < m; ++j)
+    {
+        int colSum = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            colSum += matrix[i][j];
+        }
+        if (colSum > maxSum)
+        {
+            maxSum = colSum;
+            maxColCount = 1;
+            maxCols[0] = j;
+        }
+        else if (colSum == maxSum)
+        {
+            maxCols[maxColCount] = j;
+            maxColCount++;
+        }
+    }
+
+    cout << "Maximum sum: " << maxSum << endl;
+    cout << "Indices of the maximum sum:" << endl;
+
+    for (int i = 0; i < maxRowCount; ++i)
+    {
+        cout << "Row " << maxRows[i] << " (0-based indexing)" << endl;
+    }
+    for (int i = 0; i < maxColCount; ++i)
+    {
+        cout << "Column " << maxCols[i] << " (0-based indexing)" << endl;
+    }
+
+    return 0;
+}
