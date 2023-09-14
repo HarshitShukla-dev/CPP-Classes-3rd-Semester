343735408e9cb98d38b43fce7b21f14affecd0e8
hershit-shukla <hershitshukla35@gmail.com>
2023-09-14 14:50:53 +0000

    Sep 14, 2023, 8:20 PM

--- 14-09-2023 stack and vector.cpp
+++ 14-09-2023 stack and vector.cpp
@@ -96,3 +96,37 @@ Syntax: stack_name.pop();
 4. empty() : Check if the stack is empty. Syntax: stack_name.empty();
 5. size() : Return the size of the stack. Syntax: stack_name.size();
 */
+
+//_____________________
+
+// Creating an array which has elements from 1 to 10. Traverse through the array and push the even elements into the stack. Now calculate the sum of those elements which are divisible by 4 present in the stack.
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 8, 9, 10};
+    stack<int> s;
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            s.push(arr[i]);
+        }
+    }
+    while (!s.empty())
+    {
+        int x = s.top();
+        if (x % 4 == 0)
+        {
+            sum += x;
+        }
+        s.pop();
+    }
+    cout << "Sum: " << sum << endl;
+    return 0;
+}
