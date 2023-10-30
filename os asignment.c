9e0ab397bc8b55fc16a247a52b8e273dc99ca8b6
hershit-shukla <hershitshukla35@gmail.com>
2023-10-30 14:19:42 +0000

    Oct 30, 2023, 7:49 PM

--- os asignment.c
+++ os asignment.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-void sort(int a, int b, int n){
+void sort(int a[], int b[], int n) {
     int temp;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n - i - 1; j++){
-            if(a[j] > a[j + 1]){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                // Swap elements in arrays 'a' and 'b'
                 temp = a[j];
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
@@ -15,32 +16,38 @@ void sort(int a, int b, int n){
             }
         }
     }
-
 }
 
-int main(){
-    int n;;
-    scanf("%d %d", &n, &m);
+int main() {
+    int n, m;
+    scanf("%d", &n);  // Read the number of elements in the arrays
+    scanf("%d", &m);  // Assuming you want to read 'm' from user input, though it is not used in this code.
 
     int a[n], b[n];
 
-    for(int i = 0; i < n; i++){
+    // Input values for array 'a'
+    for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-    for(int i = 0; i < n; i++){
+    // Input values for array 'b'
+    for (int i = 0; i < n; i++) {
         scanf("%d", &b[i]);
     }
 
+    // Sort arrays 'a' and 'b' based on values in 'a'
     sort(a, b, n);
 
-    for(int i = 0; i < n; i++){
+    // Print sorted arrays 'a' and 'b'
+    for (int i = 0; i < n; i++) {
         printf("%d ", a[i]);
     }
 
     printf("\n");
 
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         printf("%d ", b[i]);
     }
+
+    return 0;
 }
