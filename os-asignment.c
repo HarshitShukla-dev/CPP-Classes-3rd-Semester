17c5eee67107314d79d817fb6698e1173bf33d8d
hershit-shukla <hershitshukla35@gmail.com>
2023-10-30 14:51:06 +0000

    Oct 30, 2023, 8:21 PM

--- os-asignment.c
+++ os-asignment.c
@@ -1,65 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct Process {  
-int arrival_time;  
-int burst_time;  
-int waiting_time;  
-};  
   
-int compare(const void *a, const void *b) {  
-struct Process *p1 = (struct Process *)a;  
-struct Process *p2 = (struct Process *)b;  
-return p1->burst_time - p2->burst_time;  
-}  
-  
-int main() {  
-int n, i, j;  
-float avg_waiting_time = 0, avg_turnaround_time = 0;  
-printf("Enter the number of processes: ");  
-scanf("%d", &n);  
-struct Process processes[n];  
-for (i = 0; i< n; i++) {  
-printf("Enter arrival time and burst time of process %d: ", i+1);  
-scanf("%d %d", &processes[i].arrival_time, &processes[i].burst_time);  
-}  
-qsort(processes, n, sizeof(struct Process), compare);  
-processes[0].waiting_time = 0;  
-for (i = 1; i< n; i++) {  
-processes[i].waiting_time = 0;  
-for (j = 0; j <i; j++)   
-{   
-  
-processes[i].waiting_time += processes[j].burst_time;   
-  
-}   
-  
-avg_waiting_time += processes[i].waiting_time;   
-  
-}   
-  
-avg_waiting_time /= n;   
-  
-for (i = 0; i< n; i++) {   
-  
-avg_turnaround_time += processes[i].burst_time + processes[i].waiting_time;   
-  
-}   
-  
-avg_turnaround_time /= n;   
-  
-printf("\nProcess\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");   
-  
-for (i = 0; i< n; i++) {   
-  
-printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i+1, processes[i].arrival_time, processes[i].burst_time, processes[i].waiting_time, processes[i].burst_time+processes[i].waiting_time);   
-  
-}   
-  
-printf("\nAverage Waiting Time: %f\n", avg_waiting_time);   
-  
-printf("Average Turnaround Time: %f\n", avg_turnaround_time);   
-  
-return 0;   
-  
-}  
+int main() 
+{
+      int arrival_time[10], burst_time[10], temp[10];
+      int i, smallest, count = 0, time, limit;
+      double wait_time = 0, turnaround_time = 0, end;
+      float average_waiting_time, average_turnaround_time;
+      printf("nEnter the Total Number of Processes:t");
+      scanf("%d", &limit); 
+      printf("nEnter Details of %d Processesn", limit);
+      for(i = 0; i < limit; i++)
+      {
+            printf("nEnter Arrival Time:t");
+            scanf("%d", &arrival_time[i]);
+            printf("Enter Burst Time:t");
+            scanf("%d", &burst_time[i]); 
+            temp[i] = burst_time[i];
+      }
+      burst_time[9] = 9999;  
+      for(time = 0; count != limit; time++)
+      {
+            smallest = 9;
+            for(i = 0; i < limit; i++)
+            {
+                  if(arrival_time[i] <= time && burst_time[i] < burst_time[smallest] && burst_time[i] > 0)
+                  {
+                        smallest = i;
+                  }
+            }
+            burst_time[smallest]--;
+            if(burst_time[smallest] == 0)
+            {
+                  count++;
+                  end = time + 1;
+                  wait_time = wait_time + end - arrival_time[smallest] - temp[smallest];
+                  turnaround_time = turnaround_time + end - arrival_time[smallest];
+            }
+      }
+      average_waiting_time = wait_time / limit; 
+      average_turnaround_time = turnaround_time / limit;
+      printf("nnAverage Waiting Time:t%lfn", average_waiting_time);
+      printf("Average Turnaround Time:t%lfn", average_turnaround_time);
+      return 0;
+}
