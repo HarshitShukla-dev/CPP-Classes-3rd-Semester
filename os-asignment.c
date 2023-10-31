b6ee4f14623d770991944982563916aa66499e2a
hershit-shukla <hershitshukla35@gmail.com>
2023-10-31 15:30:11 +0000

    Oct 31, 2023, 9:00 PM

--- os-asignment.c
+++ os-asignment.c
@@ -1,64 +1,55 @@
-#include <stdio.h>
-
-int main() {
-    int arrival_time[10], burst_time[10], temp[10];
-    int i, smallest, count = 0, time, limit;
-    double wait_time = 0, turnaround_time = 0, end;
-    float average_waiting_time, average_turnaround_time;
-
-    printf("Enter the Total Number of Processes: ");
-    scanf("%d", &limit);
-    printf("\nEnter Details of %d Processes\n", limit);
-
-    for (i = 0; i < limit; i++) {
-        printf("Process %d:\n", i + 1);
-
-        do {
-            printf("Enter Arrival Time: ");
-            scanf("%d", &arrival_time[i]);
-
-            if (arrival_time[i] == 0) {
-                printf("Arrival time cannot be zero. Please enter a non-zero arrival time.\n");
-            }
-
-        } while (arrival_time[i] == 0);
-
-        printf("Enter Burst Time: ");
-        scanf("%d", &burst_time[i]);
-        temp[i] = burst_time[i];
+// SJF scheduling program in c
+#include<string.h>
+int main()
+{
+    int bt[20],at[10],n,i,j,temp,st[10],ft[10],wt[10],ta[10];
+    int totwt=0,totta=0;
+    double awt,ata;
+    char pn[10][10],t[10];
+    //clrscr();
+    printf("Enter the number of process:");
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    {
+        printf("Enter process name, arrival time& burst time:");
+        scanf("%s%d%d",pn[i],&at[i],&bt[i]);
     }
-
-    burst_time[9] = 9999;
-
-    printf("\nP\tAT\tBT\n");
-
-    for (time = 0; count != limit; time++) {
-        smallest = 9;
-        for (i = 0; i < limit; i++) {
-            if (arrival_time[i] <= time && burst_time[i] < burst_time[smallest] && burst_time[i] > 0) {
-                smallest = i;
+    for(i=0; i<n; i++)
+        for(j=0; j<n; j++)
+        {
+            if(bt[i]<bt[j])
+            {
+                temp=at[i];
+                at[i]=at[j];
+                at[j]=temp;
+                temp=bt[i];
+                bt[i]=bt[j];
+                bt[j]=temp;
+                strcpy(t,pn[i]);
+                strcpy(pn[i],pn[j]);
+                strcpy(pn[j],t);
             }
         }
-        burst_time[smallest]--;
-        if (burst_time[smallest] == 0) {
-            count++;
-            time += 2;
-            end = time;
-            wait_time = wait_time + end - arrival_time[smallest] - temp[smallest];
-            turnaround_time = turnaround_time + end - arrival_time[smallest];
-        }
+    for(i=0; i<n; i++)
+    {
+        if(i==0)
+            st[i]=at[i];
+        else
+            st[i]=ft[i-1];
+        wt[i]=st[i]-at[i];
+        ft[i]=st[i]+bt[i];
+        ta[i]=ft[i]-at[i];
+        totwt+=wt[i];
+        totta+=ta[i];
     }
-
-    average_waiting_time = wait_time / limit;
-    average_turnaround_time = turnaround_time / limit;
-
-    for (i = 0; i < limit; i++) {
-        printf("P%d\t%d\t%d\n", i + 1, arrival_time[i], temp[i]);
+    awt=(double)totwt/n;
+    ata=(double)totta/n;
+    printf("\nProcessname\tarrivaltime\tbursttime\twaitingtime\tturnaroundtime");
+    for(i=0; i<n; i++)
+    {
+        printf("\n%s\t%5d\t\t%5d\t\t%5d\t\t%5d",pn[i],at[i],bt[i],wt[i],ta[i]);
     }
-
-    printf("\nAverage Waiting Time: %.2f\n", average_waiting_time);
-    printf("Average Turnaround Time: %.2f\n", average_turnaround_time);
-    printf("Total time taken by processor to complete all the jobs: %.2f\n", end);
-
+    printf("\nAverage waiting time: %f",awt);
+    printf("\nAverage turnaroundtime: %f",ata);
     return 0;
 }
