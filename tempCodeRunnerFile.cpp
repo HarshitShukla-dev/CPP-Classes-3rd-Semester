6a636cc6754c2e3a224b931b5cc3f76b61bc6603
hershit-shukla <hershitshukla35@gmail.com>
2023-12-25 09:25:18 +0000

    Dec 25, 2023, 2:55 PM

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include <stack>
-
 using namespace std;
 
 int main()
 {
-    string s = "goodcoder";
-    stack<char> st;
-    for (int i = 0; i < s.length(); i++)
+    int arr[3][4] = {{1, 2, 3, 4},
+                     {5, 6, 7, 8},
+                     {9, 0, 1, 2}};
+
+    // Printing 2d Array
+    for (int i = 0; i < 3; i++) // Row
     {
-        st.push(s[i]);
+        for (int j = 0; j < 4; j++) // Column
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
-    while (!st.empty())
-    {
-        cout << st.top();
-        st.pop();
-    }
+
     return 0;
 }
