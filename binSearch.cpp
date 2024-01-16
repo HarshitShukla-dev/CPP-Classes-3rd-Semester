d8453fa6cc53e3761f3cd43e40d669e3b5f76653
hershit-shukla <hershitshukla35@gmail.com>
2024-01-16 13:21:06 +0530

    Update settings.json and binSearch.cpp

--- binSearch.cpp
+++ binSearch.cpp
@@ -3,19 +3,26 @@
 using namespace std;
 
 int search(vector<int>& nums, int target) {
-    int low = 0, high = nums.size()-1;
-    while (low <= high) {
-    int mid = low + (high - low) / 2;
+    int start = 0, end = nums.size()-1;
+    while (start <= end) {
+    int mid = start + (end - start) / 2;
 
     if (nums[mid] == target)
       return mid;
 
     if (nums[mid] < target)
-      low = mid + 1;
+      start = mid + 1;
 
     else
-      high = mid - 1;
+      end = mid - 1;
   }
 
   return -1;        
     }
+
+int main(){
+    vector<int> nums = {4,5,6,7,8,9,10,0,1,2,3};
+    cout << search(nums, 1);
+    return 0;
+
+}
